94288cb02a70b4ce9e009d53843ec0ab2d049886
havorx <55874008+jazzkatxlemon@users.noreply.github.com>
2022-04-21 01:23:48 +0700

    fix cors

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ const corsOptions = {
 };
 
 app.use(logger('dev'));
-app.use(cors(/*corsOptions*/));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser(process.env.COOKIE_SECRET));
